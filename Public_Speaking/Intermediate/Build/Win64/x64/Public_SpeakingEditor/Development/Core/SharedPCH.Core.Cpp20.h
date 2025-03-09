0f8a710b779e20dacd55fc9447c1bf7a82626a95
Kio Svensson <kioleesvensson@gmail.com>
2025-03-09 12:14:37 +0100

    6

---
+++ Public_Speaking/Intermediate/Build/Win64/x64/Public_SpeakingEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "D:/Projects/Unreal_Projects/Public_Speaking/Public_Speaking/Intermediate/Build/Win64/x64/Public_SpeakingEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ Public_Speaking/Intermediate/Build/Win64/x64/Public_SpeakingEditor/Development/Core/SharedPCH.Core.Project.ValApi.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33144
+#include "D:/Projects/Unreal_Projects/Public_Speaking/Public_Speaking/Intermediate/Build/Win64/x64/Public_SpeakingEditor/Development/Core/SharedPCH.Core.Project.ValApi.Cpp20.h"
