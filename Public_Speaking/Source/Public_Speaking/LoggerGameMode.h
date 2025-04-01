3f06c6cd28d228d7fb7a0bf87470a563314b2361
Kio Svensson <kioleesvensson@gmail.com>
2025-04-01 14:16:47 +0200

    Added a logger to log input and output

---
+++ Public_Speaking/Source/Public_Speaking/LoggerGameMode.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LoggerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUBLIC_SPEAKING_API ALoggerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	TArray<FString> Messages;
+	FString FileName = "log";
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Logger") 
+	void AddMessage(const FString& Message);
+
+	UFUNCTION(BlueprintCallable)
+	void SaveFileToFile() const; 
+	
+};
